7fc3e8124972f470da402062a34d47523ecda0e1
rohit <rohitindurke@gmail.com>
2025-07-14 22:45:41 +0530

    UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
-'use client'; // only needed in /app directory; safe to skip here
-
-import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [price, setPrice] = useState(null);
-
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      const res = await fetch('api/price'); // calling your own API
-      const data = await res.json();
-      setPrice(data.price);
-    }, 5000); // every 5 sec
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <main>
-      <h1>Live BTC Price</h1>
-      <p>BTC/USDT: {price ? `$${price}` : 'Loading...'}</p>
-    </main>
+    <div className="tereNam">
+      <center>
+
+        nav bar
+      </center>
+    </div>
   );
 }
